1013ef8c8513c32675cb150ad85bc5b60d26cf06
Pratim Mukherjee <pratimmukherjeeiem@gmail.com>
2021-04-29 18:28:26 +0530

    rectifying the mistake

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php include_once ("./public/index.html")?>
-<?php include_once ("./public/main.js")?>
-<?php include_once ("./public/style.css")?>
-<?php include_once ("index.js")?>
+<!-- <?php include_once ("./public/main.js")?> -->
+<!-- <?php include_once ("./public/style.css")?> -->
+<!-- <?php include_once ("index.js")?> --> -->
